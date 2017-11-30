f287bbcd1d020facd7aa4e194a9781673c4786c8
Miguel Angel Barrera Muñoz <mbmdevelop@gmail.com>
2017-11-30 14:27:50 +0100

    FirstCommit

---
+++ RoundedFrameUWP.UWP/RoundedFrameRenderer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+using RoundedFrameUWP.UWP;
+using Xamarin.Forms.Platform.UWP;
+using RoundedFrameUWP;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(RoundedFrame), typeof(RoundedFrameRenderer))]
+namespace RoundedFrameUWP.UWP
+{
+	public class RoundedFrameRenderer : FrameRenderer
+	{
+		private double _radiusCorner = 15;
+		private Windows.UI.Color _frameBg;
+
+		protected override void OnElementChanged(ElementChangedEventArgs<Frame> e)
+		{
+			base.OnElementChanged(e);
+			var myFrame = Element as RoundedFrame;
+
+			if (Control != null)
+			{
+				
+				if (myFrame.CornerRadius != -1 && myFrame.CornerRadius != 0)
+				{
+					_radiusCorner = myFrame.CornerRadius;
+				}
+
+				var frame = e.NewElement;
+				_frameBg = Windows.UI.Color.FromArgb(
+					(byte)(frame.BackgroundColor.A * 255),
+					(byte)(frame.BackgroundColor.R * 255),
+					(byte)(frame.BackgroundColor.G * 255),
+					(byte)(frame.BackgroundColor.B * 255));
+
+
+				CornerRadius corner;
+				switch (myFrame.RoundedStyle)
+				{
+					case RoundedFrame.RoundedStyleEnum.Full:
+						corner = new Windows.UI.Xaml.CornerRadius(_radiusCorner);
+						break;
+					case RoundedFrame.RoundedStyleEnum.Down:
+						corner = new Windows.UI.Xaml.CornerRadius(_radiusCorner, _radiusCorner, 0, 0);
+						break;
+					case RoundedFrame.RoundedStyleEnum.Top:
+						corner = new Windows.UI.Xaml.CornerRadius(0, 0, _radiusCorner, _radiusCorner);
+						break;
+					default:
+						corner = new CornerRadius(_radiusCorner);
+						break;
+				}
+
+				Control.CornerRadius = corner;
+
+
+
+
+				Control.Background = new SolidColorBrush(_frameBg);
+				frame.BackgroundColor = Xamarin.Forms.Color.Transparent;
+			}
+
+		}
+	}
+}
---
+++ RoundedFrameUWP/RoundedFrame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RoundedFrameUWP
+{
+	public class RoundedFrame : Frame
+	{
+		
+
+		public RoundedStyleEnum RoundedStyle { get; set; }
+		public double RadiusCorner { get; set; }
+
+		public enum RoundedStyleEnum
+		{
+			Full,
+			Top,
+			Down
+		}
+	}
+}
